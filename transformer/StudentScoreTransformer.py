b97b457266c29b5d23e6a3e26020f5dee0fcc5db
Siyuan <jilingou@gmail.com>
2023-04-19 14:21:16 +0800

    change source to golddb

--- transformer/StudentScoreTransformer.py
+++ transformer/StudentScoreTransformer.py
@@ -9,14 +9,14 @@ class StudentScoreTransformer:
     def read_table_and_transform(self, spark, jdbcUrl, connectionProperties):
         df_a = spark.read.jdbc(url=jdbcUrl, table="assessments", properties=connectionProperties)
         df_a = df_a.withColumn('weight', df_a['weight'] / 100)
-        df_sa = spark.read.jdbc(url=jdbcUrl, table="student_assessment", properties=connectionProperties)
+        df_sa = spark.read.jdbc(url=jdbcUrl, table="studentassessment", properties=connectionProperties)
 
         df_join = df_sa.join(df_a, on="id_assessment", how="left")
 
         processed_data = df_join.groupBy(["id_student", "code_presentation"]) \
             .agg(sum(col("score") * col("weight")).alias('weighted_score')) \
             .dropna() \
-            .select(col("id_student"), col("weighted_score")) \
+            .select(col("id_student").cast("varchar(20)"), col("weighted_score")) \
             .sort(desc("weighted_score")) \
             .toPandas()
 
